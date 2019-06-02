a8fea54d70a48663ba0df2adc4474e606794a717
katherine <kmarmitage@gmail.com>
2019-06-02 15:56:39 -0700

    got spotify object working

--- liri.js
+++ liri.js
@@ -1,5 +1,18 @@
+
+//require env file to handle environment variables
+
 require("dotenv").config();
 
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify); //access the keys this way
+//create spotify object to hold environment variables
+
+function Spotify(id, secret) {
+    this.id = id;
+    this.secret = secret;
+}
+
+var spotify = new Spotify(keys.spotify.id, keys.spotify.secret); //access the keys this way
+
+//create an array to hold the possible command line arguments
+var inputArray = ['concert-this', 'spotify-this-song', 'movie-this', 'do-what-it-says'];
